78112c93306979d5bf584711e8a14ca5a006ac59
Nambukeerthi <nambu935@gmail.com>
2024-09-30 01:47:31 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -120,7 +120,7 @@ elif select == "DATA EXPLORATION":
              states_name.append(feature["properties"]["ST_NM"])
                     
             states_name.sort()
-            fig_india_1 = px.choropleth(tacyg_test, geojson = data1, locations= "States", featureidkey = "properties.ST_NM", color = " Transaction_amount", color_continues_scale = "rainbow", range_color = tacyg_test["Transaction_amount "].min(),tacyg_test["Transaction_amount "].max()), hover_name = "States", title = f"{year} "TRANSACTION AMOUNT",fitbounds ="locations",height = 600, width = 600)    
+            fig_india_1 = px.choropleth(tacyg_test, geojson = data1, locations= "States", featureidkey = "properties.ST_NM", color = "Transaction_amount", color_continues_scale = "Rainbow", range_color = (tacyg_test["Transaction_amount"].min(), tacyg_test["Transaction_amount"].max()), hover_name = "States", title = f"{year} "TRANSACTION AMOUNT", fitbounds ="locations", height = 600, width = 600)    
             st.plotly_chart(fig_india_1, use_container_width=True)   
             
         elif method_1 == "Aggrecated transaction":
