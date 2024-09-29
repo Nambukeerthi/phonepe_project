44909301db6c75aedd87e28e37d8b1a219e4f16b
Nambukeerthi <nambu935@gmail.com>
2024-09-29 23:05:50 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -113,7 +113,7 @@ elif select == "DATA EXPLORATION":
             data1 = json.loads(response.content)
             states_name =[]
             for future in data1["features"]:
-            states_name.append(feature["properties"]["ST_NM"])
+             states_name.append(feature["properties"]["ST_NM"])
             states_name.sort()
             fig_india_1 = px.choropleth(tacyg, geojason = data1, locations= "States", featureidkey = "properties.ST_NM", color = " Transaction_amount", color_continues_scale = "rainbow", range_color = tacyg["Transaction_amount "].min(),tacyg["Transaction_amount "].max()), hover_name = "States", title = f"{year} "TRANSACTION AMOUNT",fitbounds ="locations",height = 600, width = 600)    
             st.plotly_chart(fig_india_1, use_container_width=True)   
