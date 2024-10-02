50a390e2212edb18290279b38d9b3d351a74d82d
Nambukeerthi <nambu935@gmail.com>
2024-10-03 01:00:40 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -212,7 +212,7 @@ elif select == "TOP CHARTS":
         fig_q1_asce = px.bar(q1_asce , x = "States", y = "sum", title = "TRANSACTION AMOUNT")
         st.plotly_chart(fig_q1_asce, theme=None, use_container_width=True)   
         st.dataframe(q1_sum , use_container_width=True)
-        fig_q1_desc = px.barq1_desc , x = "States", y = "sum", title = "TRANSACTION AMOUNT")
+        fig_q1_desc = px.bar(q1_desc , x = "States", y = "sum", title = "TRANSACTION AMOUNT")
         st.plotly_chart(fig_q1_desc, theme=None, use_container_width=True)  
 
 
