32d39dfd670f8a2faaef35c639742fb633389496
Nambukeerthi <nambu935@gmail.com>
2024-10-06 15:36:34 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -42,11 +42,11 @@ def top_charts_q1():
         
        q1_desc = group1.sort_values(by="Transaction_amount", ascending=False).head(10) 
        q1_desc.reset_index(drop= True, inplace=True)
-       fig_q1_desc = px.line(q1_desc , x = "States", y = "Transaction_amount", title = "LOWEST", color_continuous_scale=px.colors.sequential.Viridis)
+       fig_q1_desc = px.line(q1_desc , x = "States", y = "Transaction_amount", title = "LOWEST")
        st.plotly_chart(fig_q1_desc, theme=None, use_container_width=True)
         
        q1_avg = df_q1.groupby("States")["Transaction_amount"].mean().reset_index()
-       fig_q1_avg = px.bar(q1_avg , x = "States", y = "Transaction_amount", title = "AVERAGE", color_continuous_scale=px.colors.sequential.Aggrnyl)
+       fig_q1_avg = px.bar(q1_avg , x = "States", y = "Transaction_amount", title = "AVERAGE")
        st.plotly_chart(fig_q1_avg, theme=None, use_container_width=True) 
         
       
