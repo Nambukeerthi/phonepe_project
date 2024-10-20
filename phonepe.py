9d3156d919255fb2618604ee6d9d0907760f7c20
Nambukeerthi <nambu935@gmail.com>
2024-10-21 00:04:35 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -281,7 +281,7 @@ def Top_insurance(df_csv):
     
 def top_ins_pin(df_csv):
         df2 = df_csv
-        pincode = st.selectbox ("Select the Pincode",df2["States"].unique()
+        pincode = st.selectbox ("Select the Pincode",df2["States"].unique())
         tip = df2[df2["States"] == pincode ]
         tip.reset_index(drop= True, inplace=True) #inplace- store the data in same variable
         tipg = tip.groupby("Pincodes")[["Transaction_count","Transaction_amount"]].sum()
