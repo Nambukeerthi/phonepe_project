bb2d9104a0cf62cd3528dddb460b4b93b7513406
Nambukeerthi <nambu935@gmail.com>
2024-10-20 13:08:36 +0530

    Update phonepe.py

--- phonepe.py
+++ phonepe.py
@@ -187,10 +187,10 @@ def map_ins_dist(df_csv):
         st.subheader("STATEWISE TRANSACTION")
         df2 = df_csv  
         
-        for i, state in enumerate(df2["States"].unique()):
-           state = st.selectbox(f"Select the State {i+1}", df2["States"].unique(), key=f"state_selectbox_{i}")
+        #for i, state in enumerate(df2["States"].unique()):
+           #state = st.selectbox(f"Select the State {i+1}", df2["States"].unique(), key=f"state_selectbox_{i}")
         
-        #state = st.selectbox ("Select the State",df2["States"].unique())
+        state = st.selectbox ("Select the State",df2["States"].unique(), key="state_selectbox_1")
         midy = df2[df2["States"] == state ]
         midy.reset_index(drop= True, inplace=True) #inplace- store the data in same variable
         midyg = midy.groupby("Districts")[["Transaction_count","Transaction_amount"]].sum()
